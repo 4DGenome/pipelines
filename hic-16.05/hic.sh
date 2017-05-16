0d746e074c5a23ddd98f306e309d58bd4819e4c9
jaquol <javier.quilez@crg.eu>
2017-05-16 11:08:20 +0200

    add activate python environment

--- hic-16.05/hic.sh
+++ hic-16.05/hic.sh
@@ -18,6 +18,7 @@ main() {
 	job_name=$pipeline_name-$pipeline_version
 
 	# makes that the job uses this python/tadbit
+	source activate tadbit_old
 	export PATH="/software/mb/el7.2/anaconda2/bin:$PATH"
 
 	# pipeline scripts

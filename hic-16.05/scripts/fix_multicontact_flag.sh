ac2ffd67d90c1b14494799f9612d0aa57a89a173
jaquol <javier.quilez@crg.eu>
2018-01-23 13:57:41 +0100

    additional threads to fix_multicontact_flag.sh

--- hic-16.05/scripts/fix_multicontact_flag.sh
+++ hic-16.05/scripts/fix_multicontact_flag.sh
@@ -4,19 +4,20 @@
 
 oldbam=$1
 newbam=$2
+slots=$3
 
 # do the magic
 
 echo "Changing multicontac tag"
 
-samtools view -h $oldbam | \
+samtools view -h -@ $slots $oldbam | \
 	awk -F "\t" -v OFS="\t" \
 		'{n = gsub("#", "#", $1); if(n > 0){$12 = "TC:i:2"}else{$12 = "TC:i:1"} print}' |
-	samtools view -Sb - > \
+	samtools view -Sb -@ $slots - > \
 			 $newbam
 
 echo "Indexing bam"
 
-samtools index $newbam
+samtools index -@ $slots $newbam
 
 echo "Done!"
